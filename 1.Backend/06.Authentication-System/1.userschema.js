6fc0bfe8509f4c1662972b473c5e129030b9ac9d
beautyDML <beauty.rani@digimantra.com>
2026-06-15 16:31:03 +0530

    user schema

---
+++ 1.Backend/06.Authentication-System/1.userschema.js
@@ -0,0 +1,59 @@
+// user schema: it is all about what information do i need about user.
+// basic information:
+// name,email,pass,phone number, role, profile picture, created at
+
+const mongoose = require("mongoose");
+
+const userschema = new mongoose.schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true //remove extra space 
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    phone: {
+      type: Number,
+      required: true,
+      max: 10
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"], //enums means only allowed value
+      required: true
+    },
+    avatar: {
+      type: String,
+      default: " "
+    },
+  },
+  { timestamps: true }, //mongo db automatically creates createdAt and updateAt 
+);
+
+module.exports=mongoose.model("user",schema)
+
+//folder struct:
+// project/
+// │
+// ├── models/
+// │   └── user.model.js
+// │
+// ├── controllers/
+// ├── routes/
+// ├── middleware/
+// └── server.js
+
+
+
+//improvement : always try to create model file like this; user.model.js student.model.js
+// 

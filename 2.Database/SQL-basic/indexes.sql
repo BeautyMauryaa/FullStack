23fb11c6866353f1ea1314002632be202ce2c1fb
beautyDML <beauty.rani@digimantra.com>
2026-07-06 12:53:01 +0530

    indexes

---
+++ 2.Database/SQL-basic/indexes.sql
@@ -0,0 +1,20 @@
+-- index work same as mongodb 
+CREATE index idx_email
+on employees(email);
+
+-- now when we search:
+SELECT * FROM employees,
+WHERE email="beauty@gmail.com"
+-- flow: query -> index -> row pointer -> actual row 
+-- instead of checking every row 
+
+-- most sql db use B-tree by default to store the index
+--                 M
+
+--           /           \
+
+--       G                  T
+
+--     /   \              /    \
+
+--   C      J          P        Z

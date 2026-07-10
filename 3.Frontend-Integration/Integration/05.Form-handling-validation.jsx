f834c08a0f46266ed61549bfdd0a6c00853d895b
beautyDML <beauty.rani@digimantra.com>
2026-07-10 18:30:59 +0530

    form handling

---
+++ 3.Frontend-Integration/Integration/05.Form-handling-validation.jsx
@@ -0,0 +1,6 @@
+// form handling: reading user input, validating input, updating states, submitting data to backend
+const [form ,setForm] = useState({
+    name:"",
+    email:"",
+    phone:""
+})

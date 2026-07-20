4af9b684f6bc7a787055decc465de0f6d733cb46
beautyDML <beauty.rani@digimantra.com>
2026-07-20 18:31:19 +0530

    updated db

--- Allegory/backend/src/config/db.js
+++ Allegory/backend/src/config/db.js
@@ -1,30 +1,15 @@
 import mongoose from "mongoose";
 
-// const connectDB=async()=>{
-//     try{
-//         await mongoose.connect(process.env.MONGOURL);
-//         console.log("mongodb connected to server.");
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGOURL);
+         console.log("MongoDB connected successfully");
 
-//     }catch(err){
-//         console.log("mongodb connection failed", err.message);
-//         process.exit(1);
+    }catch(err){
+        console.log("mongodb connection failed", err.message);
+        process.exit();
 
         
-//     }
-// };
-const connectDB = async () => {
-    try {
-
-        console.log("MONGOURL:", process.env.MONGOURL);
-
-        await mongoose.connect(process.env.MONGOURL);
-
-        console.log("MongoDB connected successfully");
-
-    } catch (err) {
-        console.log("MongoDB connection failed:", err.message);
-        process.exit(1);
     }
 };
-
 export default connectDB;
